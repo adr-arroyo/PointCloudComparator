9c8d139589bd397e26e8ad4eae5d7cd36a07707d
adr-arroyo <adr.arroyo.perez@gmail.com>
2017-07-10 10:26:27 +0200

    Working code but poor results

---
+++ include/comparator.h
@@ -0,0 +1,54 @@
+/*
+ * comparator.hpp
+ *
+ *  Created on: 24 jun. 2017
+ *      Author: Adrian Arroyo - adr.arroyo.perez@gmail.com
+
+ */
+
+#ifndef INCLUDE_COMPARATOR_H_
+#define INCLUDE_COMPARATOR_H_
+
+#include <iostream>
+#include <boost/thread/thread.hpp>
+#include <pcl/range_image/range_image.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/visualization/range_image_visualizer.h>
+#include <pcl/visualization/pcl_visualizer.h>
+#include <pcl/features/range_image_border_extractor.h>
+#include <pcl/keypoints/narf_keypoint.h>
+#include <pcl/features/narf_descriptor.h>
+#include <pcl/console/parse.h>
+#include <pcl/point_types.h>
+#include <pcl/features/pfh.h>
+#include <pcl/kdtree/kdtree_flann.h>
+#include <pcl/surface/gp3.h>
+#include <pcl/features/spin_image.h>
+#include <cmath>
+#include <pcl/registration/icp.h>
+#include <pcl/keypoints/sift_keypoint.h>
+#include <vector>
+#include <pcl/features/vfh.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/features/intensity_gradient.h>
+#include <pcl/features/rift.h>
+#include <pcl/filters/voxel_grid.h>
+#include<pcl/point_types_conversion.h>
+#include <pcl/features/fpfh.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/octree/octree.h>
+#include "../include/segmentation.h"
+
+void processNARF(std::string filename, std::string filename2);
+
+pcl::PointCloud<pcl::PointWithScale> processSift(
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_xyz_ptr,
+		pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_xyz_ptr2);
+
+void processVHF(std::string filename, std::string filename2);
+
+void processRIFT(std::string filename, std::string filename2);
+
+void processFPFH(std::string filename, std::string filename2);
+
+#endif /* INCLUDE_COMPARATOR_H_ */
---
+++ include/segmentation.h
@@ -0,0 +1,41 @@
+/*
+ * segmentation.hpp
+ *
+ *  Created on: 24 jun. 2017
+ *      Author: Adrian Arroyo - adr.arroyo.perez@gmail.com
+
+ */
+
+#ifndef INCLUDE_SEGMENTATION_H_
+#define INCLUDE_SEGMENTATION_H_
+
+#include <pcl/console/parse.h>
+#include <pcl/ModelCoefficients.h>
+#include <pcl/point_types.h>
+#include <pcl/io/ply_io.h>
+#include <pcl/filters/extract_indices.h>
+#include <pcl/filters/voxel_grid.h>
+#include <pcl/features/normal_3d.h>
+#include <pcl/kdtree/kdtree.h>
+#include <pcl/sample_consensus/method_types.h>
+#include <pcl/sample_consensus/model_types.h>
+#include <pcl/segmentation/sac_segmentation.h>
+#include <pcl/segmentation/extract_clusters.h>
+#include <pcl/visualization/cloud_viewer.h>
+#include <pcl/filters/passthrough.h>
+#include <pcl/segmentation/region_growing_rgb.h>
+#include <pcl/segmentation/region_growing.h>
+#include <pcl/segmentation/progressive_morphological_filter.h>
+
+int planar_segmentation(std::string filename);
+
+int cluster_segmentation(std::string filename);
+
+int color_growing_segmentation(std::string filename);
+
+std::vector<pcl::PointCloud<pcl::PointXYZRGB>::Ptr> region_growing_segmentation(
+		std::string filename);
+
+int ground_segmentation(std::string filename);
+
+#endif /* INCLUDE_SEGMENTATION_H_ */
